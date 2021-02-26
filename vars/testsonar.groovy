898181061de035eadf720505515c60f163f7ed5a
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-02-26 17:05:55 +0530

    Update testsonar.groovy

--- vars/testsonar.groovy
+++ vars/testsonar.groovy
@@ -1,4 +1,10 @@
 def call() {
+  
+steps {
+withSonarQubeEnv('SonarQube') { //there must be a tool named 'sonar' configured on Jenkins (/jenkins/configureTools)
+sh 'mvn sonar:sonar' //publish project quality on sonar
+}
+}
 timeout(time: 3, unit: 'MINUTES') {
 echo "Initializing quality gates..."
 sh 'sleep 10' //small delay because project quality can still being published on previous stage (specially on bigger projects).
