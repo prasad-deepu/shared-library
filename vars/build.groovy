7868f5808ebb5481d9928f49b5942222556dac8c
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-02-26 16:51:21 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,21 +1,14 @@
-
-def call(String mvnaction) {
-    
-       if ("${mvnaction}" == "Clean")
-                    {
-                    sh "mvn clean"
-                    }
-              else if ("${mvnaction}" == "Compile")
-                    {
-                    sh "mvn clean compile"
-                    }
-               else if ("${mvnaction}" == "Test")
-                    {
-                    sh "mvn clean test"
-                    }
-                 else if ("${mvnaction}" == "Install")
-                    {
-                    sh "mvn clean install"
-                    }
-
+def call() {
+timeout(time: 3, unit: 'MINUTES') {
+echo "Initializing quality gates..."
+sh 'sleep 10' //small delay because project quality can still being published on previous stage (specially on bigger projects).
+def result = waitForQualityGate()
+//add some more logic if needed
+//this is enabled by quality gates plugin: https://wiki.jenkins.io/display/JENKINS/Quality+Gates+Plugin
+if (result.status != 'OK') {
+error "Pipeline aborted due to quality gate failure: ${result.status}"
+} else {
+echo "Quality gate passed with result: ${result.status}"
+}
+}
 }
