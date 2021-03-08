45fa0654689495435bc60d3c22ae457a4959b546
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-03-08 15:41:07 +0530

    Update testunit.groovy

--- vars/testunit.groovy
+++ vars/testunit.groovy
@@ -1,15 +1,4 @@
-
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
-
-/**
- * Unit test for simple App.
- */
+import sttaic com.mycompany.app.AppTest;
 
 def call() {
     
