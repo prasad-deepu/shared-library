9a8d53f7a38765ce3618c075c5201f21de4913d7
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-03-09 12:02:50 +0530

    Create nexus_test.groovy

---
+++ vars/nexus_test.groovy
@@ -0,0 +1,22 @@
+def call {
+    
+  nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                          )             
+    }
