44be090e8b7d8d5687a84c14d7cce57bb224fd9c
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-03-09 12:27:11 +0530

    Update nexus_test.groovy

--- vars/nexus_test.groovy
+++ vars/nexus_test.groovy
@@ -1,7 +1,8 @@
 def call( ) {
     
     
-    script.sh { 
+    script { 
+         sh "
                     pom = readMavenPom file: "pom.xml";
                     filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
                     echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
@@ -9,6 +10,7 @@ def call( ) {
                     artifactExists = fileExists artifactPath;
                     if(artifactExists) {
                         echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        "
                     }
             
     }
