426b5b4db572f46d7ab196102a7e6d6f0a5f34f8
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-03-11 10:41:21 +0530

    Update testunit.groovy

--- vars/testunit.groovy
+++ vars/testunit.groovy
@@ -2,40 +2,4 @@ import com.mycompany.app.AppTest;
 
 def call() {
     
-public class AppTest
-{
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
-
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-
-}
 }
