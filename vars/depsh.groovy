21c2a89f3fd144e38c975936477479caa67d329d
elonmusk408 <46572149+elonmusk408@users.noreply.github.com>
2021-04-07 17:17:39 +0530

    Update depsh.groovy

--- vars/depsh.groovy
+++ vars/depsh.groovy
@@ -1,5 +1,5 @@
 def call() {
  
-      sh 'scp -r /root/.jenkins/workspace/Simple-WebApp-Deploy/target/*.war root@52.152.229.140:/root/tool/apache-tomcat-8.5.64/webapps/'
+      sh 'scp -r /root/.jenkins/workspace/$Jobname/target/*.war root@52.152.229.140:/root/tool/apache-tomcat-8.5.64/webapps/'
   
 }
